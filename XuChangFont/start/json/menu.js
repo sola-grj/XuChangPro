2356696e42a3f2b6cbf4af334dbe921ea19fb84a
郭睿杰 <1131631886@qq.com>
2020-08-31 11:10:13 +0800

    第一次提交

---
+++ XuChangFont/start/json/menu.js
@@ -0,0 +1,271 @@
+{
+    "code":0,
+    "msg":"",
+    "data":[
+        {
+        "name":"UserPermission"
+        ,"title":"用户权限管理子系统"
+        ,"icon":"layui-extend-quanxian"
+        ,"list":[
+            {
+            "name":"UserManage"
+            ,"title":"用户管理"
+            ,"icon":"layui-extend-wenjianjia"
+            ,"list":[
+                {
+                    "name":"UserManage"
+                    ,"title":"用户管理"
+                    ,"icon":"layui-extend-wenjian"
+                },
+                {
+                    "name":"MenuManage"
+                    ,"title":"菜单管理"
+                    ,"icon":"layui-extend-wenjian"
+                },
+                {
+                    "name":"ModuleManage"
+                    ,"title":"模块管理"
+                    ,"icon":"layui-extend-wenjian"
+                }
+                ]
+
+        },
+                {
+            "name":"PermissionManage"
+            ,"title":"权限管理"
+            ,"icon":"layui-extend-wenjianjia"
+            ,"list":[
+                {
+                "name":"PostManage"
+                ,"icon":"layui-extend-wenjian"
+                ,"title":"岗位管理"
+            },
+                {
+                "name":"RoleManage"
+                ,"icon":"layui-extend-wenjian"
+                ,"title":"角色管理"
+            },{
+                "name":"PermissionManage"
+                ,"icon":"layui-extend-wenjian"
+                ,"title":"权限管理"
+            }]
+        }]
+    },
+        {
+            "name":"DataCollectProcess"
+            ,"title":"数据归集处理子系统"
+            ,"icon": "layui-extend-guiji"
+            ,"list":[
+            {
+                "title":"待开发"
+            }
+        ]
+        },
+        {
+            "name":"DataAnalysisWarning"
+            ,"title":"税收分析预警子系统"
+            ,"icon": "layui-extend-yujingfenxi"
+            ,"list":[
+                    {
+                        "name": "FourOneAnalysis",
+                        "title": "四表一票分析",
+                        "icon":"layui-extend-wenjianjia",
+                        "list": [
+                            {
+                                "name":"ConstructionIndustry",
+                                "icon":"layui-extend-wenjian",
+                                "title":"建筑业环保税、水资源税预警"
+                            }
+                        ]
+                    },
+                    {
+                        "name": "IntelligentMining",
+                        "title": "智能挖掘",
+                        "icon":"layui-extend-wenjianjia",
+                        "list": [
+                            {
+                                "name":"ForecastDesign",
+                                "icon":"layui-extend-wenjian",
+                                "title":"虚开预测设计"
+                            }
+                        ]
+                    },
+                    {
+                        "name": "AntiFraud",
+                        "title": "防虚打骗",
+                        "icon":"layui-extend-wenjianjia",
+                        "list": [
+                            {
+                                "name":"LegalAndFinancial",
+                                "icon":"layui-extend-wenjian",
+                                "title":"异地法人、异地财务负责人"
+                            }
+                        ]
+                    },
+                    {
+                        "name": "VATInvoiceAnalysis",
+                        "title": "增值税发票分析",
+                        "icon":"layui-extend-wenjianjia",
+                        "list": [
+                            {
+                                "name":"VATInvoice",
+                                "icon":"layui-extend-wenjian",
+                                "title":"本市各月增值税发票开具取得情况"
+                            }
+                        ]
+                    },
+                    {
+                        "name": "CheckSysDataAnalysis",
+                        "title": "协查系统数据分析",
+                        "icon":"layui-extend-wenjianjia",
+                        "list": [
+                            {
+                                "name":"CheckSysDataAnalysis",
+                                "icon":"layui-extend-wenjian",
+                                "title":"协查系统数据分析"
+                            }
+                        ]
+                    },
+                    {
+                        "name": "OneFamilyAnalysis",
+                        "title": "一户式分析",
+                        "icon":"layui-extend-wenjianjia",
+                        "list": [
+                            {
+                                "name":"TaxDeposit",
+                                "icon":"layui-extend-wenjian",
+                                "title":"分征收项目品目分月税款入库情况查询"
+                            }
+                        ]
+                    },
+                    {
+                        "name": "IndustryManagement",
+                        "title": "行业管理",
+                        "icon":"layui-extend-wenjianjia",
+                        "list": [
+                            {
+                                "name":"BuildingMaterialsIndustry",
+                                "icon":"layui-extend-wenjian",
+                                "title":"建材行业同行业发票开具的价格对比"
+                            }
+                        ]
+                    },
+                    {
+                        "name": "ThirdInformationAnalysis",
+                        "title": "第三方信息分析",
+                        "icon":"layui-extend-wenjianjia",
+                        "list": [
+                            {
+                                "name":"JudicialAuction",
+                                "icon":"layui-extend-wenjian",
+                                "title":"司法拍卖关联分析"
+                            }
+                        ]
+                    },
+                    {
+                        "name": "InvoiceFullQuery",
+                        "title": "发票全量查询",
+                        "icon":"layui-extend-wenjianjia",
+                        "list": [
+                            {
+                                "name":"OutputInvoice",
+                                "icon":"layui-extend-wenjian",
+                                "title":"销项发票监控"
+                            }
+                        ]
+                    },
+                    {
+                        "name": "DeclareFullQuery",
+                        "title": "申报全量查询",
+                        "icon":"layui-extend-wenjianjia",
+                        "list": [
+                            {
+                                "name":"DeclareFullQuery",
+                                "icon":"layui-extend-wenjian",
+                                "title":"申报全量查询"
+                            }
+                        ]
+                    },
+                    {
+                        "name": "BasicInformationCollection",
+                        "title": "基础信息查询",
+                        "icon":"layui-extend-wenjianjia",
+                        "list": [
+                            {
+                                "name":"RiskIndustry",
+                                "icon":"layui-extend-wenjian",
+                                "title":"风险行业归类"
+                            }
+                        ]
+                    },
+                    {
+                        "name": "DailyCollectionAssistance",
+                        "title": "日常征管辅助",
+                        "icon":"layui-extend-wenjianjia",
+                        "list": [
+                            {
+                                "name":"DailyCollectionAssistance",
+                                "icon":"layui-extend-wenjian",
+                                "title":"日常征管辅助"
+                            }
+                        ]
+                    },
+                    {
+                        "name": "ReportAndDisplay",
+                        "title": "报表及展示",
+                        "icon":"layui-extend-wenjianjia",
+                        "list": [
+                            {
+                                "name":"LargeScreen",
+                                "icon":"layui-extend-wenjian",
+                                "title":"大屏展示"
+                            }
+                        ]
+                    }
+
+                ]
+        },
+        {
+            "name":"ExternalTaxRelatedInformation"
+            ,"title":"外部涉税信息子系统"
+            ,"icon": "layui-extend-sheshuixinxicaiji"
+            ,"list":[
+            {
+                "title":"待开发"
+            }
+        ]
+        },
+        {
+            "name":"DataVisualization"
+            ,"title":"数据大屏展示子系统"
+            ,"icon": "layui-extend-shujukeshihua"
+            ,"list":[
+            {
+                "title":"待开发"
+            }
+        ]
+        },
+        {
+            "name":"SelfHelpAnalysis"
+            ,"title":"自助可视分析子系统"
+            ,"icon": "layui-extend-zizhufenxi"
+            ,"list":[
+            {
+                "title":"待开发"
+            }
+        ]
+        },
+        {
+            "name":"RelationshipMap"
+            ,"title":"关系图谱分析子系统"
+            ,"icon": "layui-extend-tupu"
+            ,"list":[
+            {
+                "title":"待开发"
+            }
+        ]
+        }
+    ]
+
+}
+
---
+++ XuChangFont/start/json/mymenu.js
@@ -0,0 +1,105 @@
+{
+    "code":0
+    ,"msg":""
+    ,"data":[{
+        "name":"userPermission"
+        ,"title":"用户权限子系统"
+        ,"icon":"layui-icon-home"
+        ,"list":[{
+            "name":"userManage"
+            ,"title":"用户管理"
+            ,"jump":"userPermission/userManage"
+        },{
+            "title":"权限管理"
+            ,"list":[{
+                "name":"roleManage"
+                ,"title":"角色管理"
+                ,"jump":"userPermission/roleManage"
+            },{
+                "name":"rolePermission"
+                ,"title":"用户角色权限"
+                ,"jump":"userPermission/rolePermission"
+            }]
+        }]
+    },{
+        "name":"dataCollectProcess"
+        ,"title":"数据归集处理子系统"
+        ,"icon": "layui-icon-component"
+        ,"list":[{}]
+    },{
+        "name":"DataAnalysisWarning"
+        ,"title":"税收数据分析预警子系统"
+        ,"icon": "layui-icon-app"
+        ,"list":[
+                {
+                    "name": "FourOneAnalysis",
+                    "title": "四表一票分析",
+                    "jump": "DataAnalysisWarning/fourOneAnalysis"
+                },
+                {
+                    "name": "IntelligentMining",
+                    "title": "智能挖掘",
+                    "jump": "DataAnalysisWarning/IntelligentMining"
+                },
+                {
+                    "name": "AntiFraud",
+                    "title": "防虚打骗",
+                    "jump": "DataAnalysisWarning/AntiFraud"
+                },
+                {
+                    "name": "VATInvoiceAnalysis",
+                    "title": "增值税发票分析",
+                    "jump": "DataAnalysisWarning/VATInvoiceAnalysis"
+                },
+                {
+                    "name": "CheckSysDataAnalysis",
+                    "title": "协查系统数据分析",
+                    "jump": "DataAnalysisWarning/CheckSysDataAnalysis"
+                },
+                {
+                    "name": "oneFamilyAnalysis",
+                    "title": "一户式分析",
+                    "jump": "DataAnalysisWarning/oneFamilyAnalysis"
+                },
+                {
+                    "name": "IndustryManagement",
+                    "title": "行业管理",
+                    "jump": "DataAnalysisWarning/IndustryManagement"
+                },
+                {
+                    "name": "ThirdInformationAnalysis",
+                    "title": "第三方信息分析",
+                    "jump": "DataAnalysisWarning/ThirdInformationAnalysis"
+                },
+                {
+                    "name": "InvoiceFullQuery",
+                    "title": "发票全量查询",
+                    "jump": "DataAnalysisWarning/InvoiceFullQuery"
+                },
+                {
+                    "name": "DeclareFullQuery",
+                    "title": "申报全量查询",
+                    "jump": "DataAnalysisWarning/DeclareFullQuery"
+                },
+                {
+                    "name": "BasicInformationCollection",
+                    "title": "基础信息查询",
+                    "jump": "DataAnalysisWarning/BasicInformationCollection"
+                },
+                {
+                    "name": "DailyCollectionAssistance",
+                    "title": "日常征管辅助",
+                    "jump": "DataAnalysisWarning/DailyCollectionAssistance"
+                },
+                {
+                    "name": "ReportAndDisplay",
+                    "title": "报表及展示",
+                    "jump": "DataAnalysisWarning/ReportAndDisplay"
+                }
+
+            ]
+    }
+    ]
+
+}
+
